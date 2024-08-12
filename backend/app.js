c773ef65fc8c3c24ac3cb246e88205affba155b8
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-08-11 23:26:00 -0400

    Setup initial models and migrations

--- backend/app.js
+++ backend/app.js
@@ -58,8 +58,8 @@ app.use((_req, _res, next) => {
 app.use((err, _req, _res, next) => {
 	// check if error is a Sequelize error:
 	if (err instanceof ValidationError) {
-		let errors = {};
-		for (let error of err.errors) {
+		const errors = {};
+		for (const error of err.errors) {
 			errors[error.path] = error.message;
 		}
 		err.title = 'Validation error';
