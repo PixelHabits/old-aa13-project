e76649e4d249b91c637c0235361ed19ab49f8191
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-08-12 03:11:32 -0400

    Adjust start script to attempt env issue fix

--- apps/backend/config/index.js
+++ apps/backend/config/index.js
@@ -1,4 +1,9 @@
 // backend/config/index.js
+console.log('Environment Variables Loaded:');
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log('PORT:', process.env.PORT);
+
 module.exports = {
 	environment: process.env.NODE_ENV || 'development',
 	port: process.env.PORT || 8000,
