1c392c0849f23dbb92b18ef49ad18bff7ad4a18e
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-09-30 11:01:45 -0400

    Feature: Create Bookings Route

---
+++ apps/backend/routes/api/bookings.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { requireAuth } = require('../../utils/auth');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+
+module.exports = router;
