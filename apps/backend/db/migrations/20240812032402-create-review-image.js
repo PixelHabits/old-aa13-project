8bcb14beae1df9b9cae7b2f3734c140dbae6048d
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-09-30 06:46:11 -0400

    Feature: Add Validators and Associations to ReviewImages Table

--- apps/backend/db/migrations/20240812032402-create-review-image.js
+++ apps/backend/db/migrations/20240812032402-create-review-image.js
@@ -3,27 +3,31 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('ReviewImages', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      reviewId: {
-        type: Sequelize.INTEGER
-      },
-      url: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+					id: {
+						allowNull: false,
+						autoIncrement: true,
+						primaryKey: true,
+						type: Sequelize.INTEGER,
+					},
+					reviewId: {
+						type: Sequelize.INTEGER,
+						allowNull: false,
+					},
+					url: {
+						type: Sequelize.STRING,
+						allowNull: false,
+					},
+					createdAt: {
+						allowNull: false,
+						type: Sequelize.DATE,
+						defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+					},
+					updatedAt: {
+						allowNull: false,
+						type: Sequelize.DATE,
+						defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+					},
+				});
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('ReviewImages');
--- apps/backend/db/models/reviewimage.js
+++ apps/backend/db/models/reviewimage.js
@@ -11,11 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      ReviewImage.belongsTo(models.Review, {foreignKey: 'reviewId'})
     }
   }
   ReviewImage.init({
-    reviewId: DataTypes.INTEGER,
-    url: DataTypes.STRING
+    reviewId: {type:DataTypes.INTEGER, allowNull: false},
+    url: {type:DataTypes.STRING, allowNull: false, unique: true},
   }, {
     sequelize,
     modelName: 'ReviewImage',
