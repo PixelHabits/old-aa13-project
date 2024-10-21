417a4bd026df84ecc4d2841822aad241da8fdd1e
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-10-21 01:39:27 -0400

    Fix: Fix error caused by refactoring preventing seeding

--- apps/backend/db/models/index.js
+++ apps/backend/db/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const process = require('node:process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../../config/database.js`)[env];
+const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 
 let sequelize;
@@ -20,10 +20,12 @@ if (config.use_env_variable) {
 }
 
 for (const file of fs.readdirSync(__dirname).filter((file) => {
-	file.indexOf('.') !== 0 &&
+	return (
+		file.indexOf('.') !== 0 &&
 		file !== basename &&
 		file.slice(-3) === '.js' &&
-		file.indexOf('.test.js') === -1;
+		file.indexOf('.test.js') === -1
+	);
 })) {
 	const model = require(path.join(__dirname, file))(
 		sequelize,
