cc1bf7d28670eda9480d624108fe7da2b9724685
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-10-20 23:59:21 -0400

    Fix: Convert to Biome linter/formatter

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { biomePlugin } from '@pbr1111/vite-plugin-biome';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    eslint({
-      lintOnStart: true,
-      failOnError: mode === "production"
-    })
+    // eslint({
+    //   lintOnStart: true,
+    //   failOnError: mode === "production"
+    // }),
+    biomePlugin()
   ],
   // To automatically open the app in the browser whenever the server starts,
   // uncomment the following lines:
