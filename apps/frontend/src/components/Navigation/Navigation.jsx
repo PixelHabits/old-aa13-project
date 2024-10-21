79d9accba21e4d9bcd1e1f47b5526e517f338c83
Devin Alsup <147578558+PixelHabits@users.noreply.github.com>
2024-10-21 04:42:06 -0400

    Feature: Add modal menu

--- apps/frontend/src/components/Navigation/Navigation.jsx
+++ apps/frontend/src/components/Navigation/Navigation.jsx
@@ -2,47 +2,22 @@
 
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import LoginFormModal from '../LoginFormModal';
-import OpenModalButton from '../OpenModalButton';
-import SignupFormModal from '../SignupFormModal';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
 
-	let sessionLinks;
-	if (sessionUser) {
-		sessionLinks = (
-			<li>
-				<ProfileButton user={sessionUser} />
-			</li>
-		);
-	} else {
-		sessionLinks = (
-			<>
-				<li>
-					<OpenModalButton
-						buttonText='Log In'
-						modalComponent={<LoginFormModal />}
-					/>
-				</li>
-				<li>
-					<OpenModalButton
-						buttonText='Sign Up'
-						modalComponent={<SignupFormModal />}
-					/>
-				</li>
-			</>
-		);
-	}
-
 	return (
 		<ul>
 			<li>
 				<NavLink to='/'>Home</NavLink>
 			</li>
-			{isLoaded && sessionLinks}
+			{isLoaded && (
+				<li>
+					<ProfileButton user={sessionUser} />
+				</li>
+			)}
 		</ul>
 	);
 }
--- apps/frontend/src/components/Navigation/ProfileButton.jsx
+++ apps/frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,9 +1,10 @@
-// frontend/src/components/Navigation/ProfileButton.jsx
-
 import { useEffect, useRef, useState } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import OpenModalMenuItem from './OpenModalMenuItem';
 
 function ProfileButton({ user }) {
 	const dispatch = useDispatch();
@@ -11,8 +12,7 @@ function ProfileButton({ user }) {
 	const ulRef = useRef();
 
 	const toggleMenu = (e) => {
-		e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-		// if (!showMenu) setShowMenu(true);
+		e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
 		setShowMenu(!showMenu);
 	};
 
@@ -20,7 +20,7 @@ function ProfileButton({ user }) {
 		if (!showMenu) return;
 
 		const closeMenu = (e) => {
-			if (ulRef.current && !ulRef.current.contains(e.target)) {
+			if (!ulRef.current.contains(e.target)) {
 				setShowMenu(false);
 			}
 		};
@@ -30,9 +30,12 @@ function ProfileButton({ user }) {
 		return () => document.removeEventListener('click', closeMenu);
 	}, [showMenu]);
 
+	const closeMenu = () => setShowMenu(false);
+
 	const logout = (e) => {
 		e.preventDefault();
 		dispatch(sessionActions.logout());
+		closeMenu();
 	};
 
 	const ulClassName = `profile-dropdown${showMenu ? '' : ' hidden'}`;
@@ -43,16 +46,33 @@ function ProfileButton({ user }) {
 				<FaUserCircle />
 			</button>
 			<ul className={ulClassName} ref={ulRef}>
-				<li>{user.username}</li>
-				<li>
-					{user.firstName} {user.lastName}
-				</li>
-				<li>{user.email}</li>
-				<li>
-					<button type='button' onClick={logout}>
-						Log Out
-					</button>
-				</li>
+				{user ? (
+					<>
+						<li>{user.username}</li>
+						<li>
+							{user.firstName} {user.lastName}
+						</li>
+						<li>{user.email}</li>
+						<li>
+							<button type='button' onClick={logout}>
+								Log Out
+							</button>
+						</li>
+					</>
+				) : (
+					<>
+						<OpenModalMenuItem
+							itemText='Log In'
+							onItemClick={closeMenu}
+							modalComponent={<LoginFormModal />}
+						/>
+						<OpenModalMenuItem
+							itemText='Sign Up'
+							onItemClick={closeMenu}
+							modalComponent={<SignupFormModal />}
+						/>
+					</>
+				)}
 			</ul>
 		</>
 	);
